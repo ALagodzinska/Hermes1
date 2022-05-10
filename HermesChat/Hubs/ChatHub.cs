235110d00f5b9eb707e15b0496d0fdc885a3e416
ALagodzinska <anastasijalagodzinska@gmail.com>
2022-05-10 18:54:35 +0300

    Add project files.

---
+++ HermesChat/Hubs/ChatHub.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace HermesChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public static List<string> roomList = new List<string>();
+
+        public static List<string> usersList = new List<string>();
+
+        public override async Task OnConnectedAsync()
+        {
+            var userName = Context.User.Identity.Name;
+            if (!usersList.Contains(userName))
+            {
+                usersList.Add(userName);
+            }
+            await Clients.All.SendAsync("GetOnlineUsers", usersList);
+            await Clients.All.SendAsync("GetRoomList", roomList);
+        }
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var userName = Context.User.Identity.Name;
+            usersList.Remove(userName);
+            await Clients.All.SendAsync("GetOnlineUsers", usersList);
+            //await base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendMessage(string message, string room)
+        {
+            var user = Context.User.Identity.Name;
+            if (message.Length > 0)
+            {
+                await Clients.Group(room).SendAsync("ReceiveMessage", user, message).ConfigureAwait(true);
+            }
+        }
+        public async Task CreateRoom(string roomName)
+        {
+            if (!roomList.Contains(roomName))
+            {
+                roomList.Add(roomName);
+            }
+            var user = Context.User.Identity.Name;
+            await Clients.All.SendAsync("GetRoomList", roomList);
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+            await Clients.Group(roomName).SendAsync("ReceiveMessage", user, " joined to " + roomName).ConfigureAwait(true);
+        }
+    }
+}
---
+++ HermesChat/wwwroot/js/chat.js
@@ -0,0 +1,84 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+var groupName = null;
+
+//Disable the send button until connection is established.
+document.getElementById("sendButton").disabled = true;
+
+connection.on("GetOnlineUsers", function (usersList) {
+    console.log(usersList);
+    Array.from(usersList);
+    var onlineUsers = document.getElementById("user-list");
+    onlineUsers.innerHTML = "";
+    usersList.forEach(addOneUser);
+});
+function addOneUser(user) {
+    var li = document.createElement("li");
+    li.innerHTML = `<p>${user}</p>`;
+    document.getElementById("user-list").appendChild(li);
+}
+
+
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    li.textContent = `${user}: ${message}`;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var message = document.getElementById("messageInput").value;
+    
+    document.getElementById("messageInput").value = "";
+    if (groupName !== null) {
+        connection.invoke("SendMessage", message, groupName).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+    event.preventDefault();
+});
+
+document.getElementById("joinButton").addEventListener("click", function (event) {
+
+    var room = document.getElementById("room").value;
+    document.getElementById("messagesList").innerHTML = "";
+    connection.invoke("CreateRoom", room).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+connection.on("GetRoomList", function (roomList) {
+    console.log(roomList);
+    Array.from(roomList);
+    var roomWindow = document.getElementById("room-list");
+    roomWindow.innerHTML = "";
+    roomList.forEach(addOneRoom);
+});
+
+function addOneRoom(room) {
+    var li = document.createElement("li");
+    var button = document.createElement("button");
+    button.textContent = room;
+    button.onclick = function (event) {
+        document.getElementById("messagesList").innerHTML = "";
+        groupName = room;
+        connection.invoke("CreateRoom", room).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+    li.appendChild(button);
+    document.getElementById("room-list").appendChild(li);
+}
+
+function scrollDown() {
+  $('#messages_container').animate({scrollTop:$('#messages_container').prop('scrollHeight')}, 1000);
+}
+
